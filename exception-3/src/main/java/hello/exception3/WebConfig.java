2243b865e032c66d19b6bd8df60b43ffc9f6eb80
imina <leeimma@naver.com>
2024-02-25 23:37:28 +0900

    section9. 1~4

--- exception-3/src/main/java/hello/exception3/WebConfig.java
+++ exception-3/src/main/java/hello/exception3/WebConfig.java
@@ -2,13 +2,18 @@ package hello.exception3;
 
 import hello.exception3.filter.LogFilter;
 import hello.exception3.interceptor.LogInterceptor;
+import hello.exception3.resolver.MyHandlerExceptionResolver;
+import hello.exception3.resolver.UserHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,7 +26,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");//오류 페이지 경로
     }
 
-//    @Bean
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
+    }
+
+    //    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
